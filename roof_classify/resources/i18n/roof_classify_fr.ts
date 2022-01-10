d47f4f683ff09f241b9cc8543e470851efcd2fa8
Julien Moura <julien.moura@oslandia.com>
2022-01-10 20:31:33 +0100

    Add French translation

---
+++ roof_classify/resources/i18n/roof_classify_fr.ts
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR" sourcelanguage="en">
+<context>
+    <name>Form</name>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="14"/>
+        <source>Form</source>
+        <translation>Classificaiton des toits</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="44"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;PluginTitle - Version X.X.X&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="66"/>
+        <source>Classifiers</source>
+        <translation>Classification</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="72"/>
+        <source>Number of parallelized jobs:</source>
+        <translation>Nombre de traitements parallèles :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="116"/>
+        <source>Miscellaneous</source>
+        <translation>Divers</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="137"/>
+        <source>Enable debug mode.</source>
+        <translation>Activer le mode DEBUG.</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="146"/>
+        <source>Debug mode (degraded performances)</source>
+        <translation>Mode DEBUG (performances dégradées)</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="196"/>
+        <source>Version used to save settings:</source>
+        <translation>Version des paramètres :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="218"/>
+        <source>Help</source>
+        <translation>Aide en ligne</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="240"/>
+        <source>Report an issue</source>
+        <translation>Créer un ticket</translation>
+    </message>
+</context>
+<context>
+    <name>RoofClassify</name>
+    <message>
+        <location filename="../../roof_classify.py" line="179"/>
+        <source>classify roofs</source>
+        <translation>Classification des toits</translation>
+    </message>
+</context>
+</TS>
