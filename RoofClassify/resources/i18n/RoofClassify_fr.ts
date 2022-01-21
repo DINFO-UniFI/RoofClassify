6ddd952494e788bef3d59c38c9b19eb6be020316
Julien Moura <julien.moura@oslandia.com>
2022-01-21 12:50:06 +0100

    Update i18n

--- RoofClassify/resources/i18n/RoofClassify_fr.ts
+++ RoofClassify/resources/i18n/RoofClassify_fr.ts
@@ -24,42 +24,82 @@
         <translation>Nombre de traitements parallèles :</translation>
     </message>
     <message>
-        <location filename="../../gui/dlg_settings.ui" line="116"/>
+        <location filename="../../gui/dlg_settings.ui" line="120"/>
+        <source>Credits</source>
+        <translation>Crédits</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="132"/>
+        <source>Sponsored and backed by:</source>
+        <translation>Sponsorisé et financé par :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="184"/>
+        <source>Developed by:</source>
+        <translation>Développé par :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="197"/>
+        <source>Original idea and script:</source>
+        <translation>Concept et idée originaux :</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="213"/>
+        <source>Interested in funding or participating?</source>
+        <translation>Intéressé(e) pour financer ou contribuer ?</translation>
+    </message>
+    <message>
+        <location filename="../../gui/dlg_settings.ui" line="258"/>
         <source>Miscellaneous</source>
         <translation>Divers</translation>
     </message>
     <message>
-        <location filename="../../gui/dlg_settings.ui" line="137"/>
+        <location filename="../../gui/dlg_settings.ui" line="279"/>
         <source>Enable debug mode.</source>
         <translation>Activer le mode DEBUG.</translation>
     </message>
     <message>
-        <location filename="../../gui/dlg_settings.ui" line="146"/>
+        <location filename="../../gui/dlg_settings.ui" line="288"/>
         <source>Debug mode (degraded performances)</source>
         <translation>Mode DEBUG (performances dégradées)</translation>
     </message>
     <message>
-        <location filename="../../gui/dlg_settings.ui" line="196"/>
+        <location filename="../../gui/dlg_settings.ui" line="338"/>
         <source>Version used to save settings:</source>
         <translation>Version des paramètres :</translation>
     </message>
     <message>
-        <location filename="../../gui/dlg_settings.ui" line="218"/>
+        <location filename="../../gui/dlg_settings.ui" line="360"/>
         <source>Help</source>
         <translation>Aide en ligne</translation>
     </message>
     <message>
-        <location filename="../../gui/dlg_settings.ui" line="240"/>
+        <location filename="../../gui/dlg_settings.ui" line="382"/>
         <source>Report an issue</source>
         <translation>Créer un ticket</translation>
     </message>
 </context>
 <context>
-    <name>RoofClassify</name>
+    <name>RoofClassifyPlugin</name>
     <message>
-        <location filename="../../plugin_main.py" line="179"/>
-        <source>classify roofs</source>
-        <translation>Classification des toits</translation>
+        <location filename="../../plugin_main.py" line="81"/>
+        <source>Error importing Scikit Learn. Plugin disabled.</source>
+        <translation>Une erreur est survenue lors de l&apos;import de Scikit-Learn (dépendance requise). Le plugin a été désactivé.</translation>
+    </message>
+    <message>
+        <location filename="../../plugin_main.py" line="95"/>
+        <source>Dependencies satisfied</source>
+        <translation>Toutes les dépendances sont OK</translation>
+    </message>
+    <message>
+        <location filename="../../plugin_main.py" line="191"/>
+        <source>Launch classification</source>
+        <translation>Lancer une classification</translation>
+    </message>
+    <message>
+        <location filename="../../plugin_main.py" line="198"/>
+        <source>Settings</source>
+        <translation>Options</translation>
     </message>
 </context>
 </TS>
